b64c8d351cbc99b7bd72494b6af798cce52b0f30
Pedro Relvas <pedromiguelrelvas@gmail.com>
2026-02-15 15:49:20 +0000

    feat(FT-1): initial commit with BE project

---
+++ finance-tracking-backend/src/test/java/pt/pmr/finance/tracking/FinanceTrackingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.pmr.finance.tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTrackingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
